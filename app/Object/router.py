4d0df11243ade9f085787df5cadc1d47492e564d
Mahishwara <117912681+Mahishwara@users.noreply.github.com>
2024-08-23 11:12:17 +0500

    Update router.py

--- app/Object/router.py
+++ app/Object/router.py
@@ -2,11 +2,11 @@ import shutil
 
 from fastapi import APIRouter, Depends, UploadFile
 
-from app.Object.dao import ObjectDAO
-from app.Object.rb import RBObject
-from app.Object.schemas import SObject, SObjectAdd
-from app.users.dependencies import get_current_admin_user
-from app.users.models import User
+from Object.dao import ObjectDAO
+from Object.rb import RBObject
+from Object.schemas import SObject, SObjectAdd
+from users.dependencies import get_current_admin_user
+from users.models import User
 
 router = APIRouter(
     prefix='/object',
@@ -27,7 +27,7 @@ async def get_object(object_id: int) -> SObject | dict:
     return res
 
 
-@router.post("/add/", summary='Добавить новый объект аренды')
+@router.post("/add", summary='Добавить новый объект аренды')
 async def add_new_object(object: SObjectAdd, user_data: User = Depends(get_current_admin_user)) -> dict:
     check = await ObjectDAO.add(**object.dict())
     if check:
@@ -48,5 +48,5 @@ async def dell_by_id(object_id: int, user_data: User = Depends(get_current_admin
 
 @router.post('/add_photo')
 async def add_object_photo(file: UploadFile, image_name: int, user_data: User = Depends(get_current_admin_user)):
-    with open(f"app/static/images/{image_name}.webp", "wb+") as photo_obj:
+    with open(f"/home/Mahishwara/Project/app/static/images/{image_name}.webp", "wb+") as photo_obj:
         shutil.copyfileobj(file.file, photo_obj)
